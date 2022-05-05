7dde1d0dcf76cceaf29fc63dec44e0691e4dd297
jvasoya27 <vjaydeep227@gmail.com>
2022-05-05 16:48:33 -0400

    JavaDataTypes

---
+++ src/First_Java_Project.java
@@ -0,0 +1,22 @@
+
+public class First_Java_Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=10;
+		double d=10.5;
+		char c='C';
+		boolean b=true;
+		
+		String s="Datatypes";
+		
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(s);
+		
+	}
+
+}
