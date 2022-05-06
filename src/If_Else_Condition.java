a7901b612de1a68bd2694e1364b2518d275f6080
jvasoya27 <vjaydeep227@gmail.com>
2022-05-06 19:55:41 -0400

    IfElseCondition

---
+++ src/If_Else_Condition.java
@@ -0,0 +1,22 @@
+
+public class If_Else_Condition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age=20;
+		
+		if (age>=18)
+		{
+			System.out.println("Eligible for work");
+		}
+		else 
+		{	
+			System.out.println("Not Eligible for work");
+		}
+		
+		
+	}	
+		
+
+}
