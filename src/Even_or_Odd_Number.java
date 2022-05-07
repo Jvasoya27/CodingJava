c13b260c57560e7122e2b47762e5a7d72d21cbae
jvasoya27 <vjaydeep227@gmail.com>
2022-05-06 20:17:04 -0400

    EvenOrOddNumber

---
+++ src/Even_or_Odd_Number.java
@@ -0,0 +1,18 @@
+
+public class Even_or_Odd_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=10;
+		
+		if(num%2==0)
+		{
+			System.out.println("The number is EVEN");
+		}
+		else
+		{
+			System.out.println("The number is Odd");
+		}
+	}
+}
