874ca2d7456a1531a74958522bca1acef405f45d
jvasoya27 <vjaydeep227@gmail.com>
2022-05-06 20:25:23 -0400

    LargestOfTwoNumbers

---
+++ src/Largest_Of_Two_Numbers.java
@@ -0,0 +1,21 @@
+
+public class Largest_Of_Two_Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=30;
+		int b=20;
+		
+		if(a>b)
+		{
+			System.out.println("A is Larger than B");
+		}
+		else
+		{
+			System.out.println("B is Larger than A");
+		}
+		
+	}
+
+}
