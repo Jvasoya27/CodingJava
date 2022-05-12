8c6019da995d80dadfdd71cc054bb7755865794b
jvasoya27 <vjaydeep227@gmail.com>
2022-05-12 08:09:04 -0400

    swapping

---
+++ src/Swapping.java
@@ -0,0 +1,45 @@
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=20;
+		
+		System.out.println("A value before swapping :" + a);
+		System.out.println("B value before swapping :" + b);
+		
+		int c;
+		
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("A value before swapping :" + a);
+		System.out.println("b value before swapping :" + b);
+		
+		System.out.println("_________________________________________________" );
+		
+		int j=5;
+		int d=10;
+		
+		System.out.println("J value before swapping :" + j);
+		System.out.println("D value before swapping :" + d);
+		
+		int p;
+		
+		p=j;
+		j=d;
+		d=p;
+		
+		System.out.println("J value before swapping :" + j);
+		System.out.println("D value before swapping :" + d);
+		
+		
+		
+	}
+	
+	
+
+}
