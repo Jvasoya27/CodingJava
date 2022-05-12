fea77fea38f89806fb6dc29c7e2a2325f1bca583
jvasoya27 <vjaydeep227@gmail.com>
2022-05-12 08:10:00 -0400

    Break_Continue_Statement

---
+++ src/Break_Statement.java
@@ -0,0 +1,30 @@
+
+public class Break_Statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=10;i++)
+		{
+			if(i==5)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+		
+		System.out.println("______________________");
+		
+		for(int a=10;a>=1;a--)
+		{
+			if(a==6)
+			{
+				break;
+			}
+		System.out.println(a);
+		}
+	
+	
+	}
+
+}
---
+++ src/Continue_Statement.java
@@ -0,0 +1,28 @@
+
+public class Continue_Statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=10;i>=1;i--)
+		{
+			if(i==5)
+			{
+				continue;
+			}
+		System.out.println(i);
+		}
+		
+		System.out.println("_________________");
+		
+		for(int a=1;a<=10;a++)
+		{
+			if(a==5)
+			{
+				continue;
+			}
+		System.out.println(a);
+		}
+	}
+
+}
